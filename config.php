77497dd4be8d60ea46cd8bb250f3fecf1766709e
Fellipe Castro <contact@fellipecastro.com>
2017-06-25 00:14:03 -0300

    update

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DECIMALS', 2);
+define('RATES_FILE', 'rates.json');
+define('SYMBOLS_FILE', 'symbols.json');
+
+?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'config.php';
+
+$rates = json_decode(file_get_contents(RATES_FILE), true);
+$symbols = json_decode(file_get_contents(SYMBOLS_FILE), true);
+
+$from = strtoupper($_GET['from']);
+$to = strtoupper($_GET['to']);
+$value = number_format(floatval($_GET['value']), DECIMALS);
+
+$rate = $rates[$from][$to];
+
+$converted_value = number_format($value * $rate, DECIMALS);
+
+$output['original_value'] = $symbols[$from] . ' ' . $value;
+$output['converted_value'] = $symbols[$to] . ' ' . $converted_value;
+
+echo json_encode($output);
+
+?>
