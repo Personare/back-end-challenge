5c574a0e99dd8ad60de9ce7e37cbc71b1794edfd
Hugo Chalhoub <hugoleonardocm@live.com>
2017-09-27 17:38:18 -0300

    Correção de documentação

--- application/controllers/indexcontroller.php
+++ application/controllers/indexcontroller.php
@@ -84,92 +84,20 @@ class IndexController extends VanillaController
                     "Exemplo de Retorno" => array(
                         "sucesso" => "Moeda {nome_da_moeda} removida com sucesso!"
                     )
-                )
-            ),
-            "/conversoes" => array(
-                "/" => array(
-                    "Métodos" => array("GET"),
-                    "Resultado" => "Retorna um array com o total de resultados e as os objetos das conversões cadastradas",
-                    "Exemplo de Retorno" => array(
-                        "count" => 2,
-                        "results" => array(
-                            array(
-                                'id' => 1,
-                                'moeda_origem' => 'BRL - Real Brasileiro',
-                                'moeda_destino' => 'USD - U.S. Dollar',
-                                'valor' => '10.00',
-                                'cotacao' => '4.00',
-                                'valor_convertido' => '$ 2.50',
-                                'data_cadastro' => '2017-09-27 10:15:23'
-                            ),
-                            array(
-                                'id' => 1,
-                                'moeda_origem' => 'USD - U.S. Dollar',
-                                'moeda_destino' => 'BRL - Real Brasileiro',
-                                'valor' => '2.50',
-                                'cotacao' => '-4.00',
-                                'valor_convertido' => 'R$ 10.00',
-                                'data_cadastro' => '2017-09-27 10:15:23'
-                            )
-                        )
-                    )
                 ),
-                "/view/{id}" => array(
-                    "Métodos" => array("GET"),
-                    "Resultado" => "Retorna um objeto com os dados da conversão buscada através do seu id",
-                    "Exemplo de Retorno" => array(
-                        'id' => 1,
-                        'moeda_origem' => 'BRL - Real Brasileiro',
-                        'moeda_destino' => 'USD - U.S. Dollar',
-                        'valor' => '10.00',
-                        'cotacao' => '4.00',
-                        'valor_convertido' => '$ 2.50',
-                        'data_cadastro' => '2017-09-27 10:15:23'
-                    ),
-                ),
-                "/add" => array(
+                "/converter/{moeda_origem_id}/{moeda_destino_id}" => array(
                     "Métodos" => array("POST"),
-                    "Resultado" => "Cadastra uma conversão no banco de dados retornando o objeto cadastrado ou um objeto de erro caso os dados estejam incorretos",
+                    "Resultado" => "Converte o valor enviado por parametro baseado na cotação enviada por parametro entre as moedas informadas pelos ids",
                     "Exemplo de Requisição" => array(
-                        'moeda_origem' => 1,
-                        'moeda_destino' => 2,
                         'valor' => '10.00',
                         'cotacao' => '4.00'
                     ),
                     "Exemplo de Retorno" => array(
-                        'id' => 1,
-                        'moeda_origem' => 'BRL - Real Brasileiro',
+                        'moeda_origem' => 'BRL - Brazillian Real',
                         'moeda_destino' => 'USD - U.S. Dollar',
                         'valor' => '10.00',
                         'cotacao' => '4.00',
                         'valor_convertido' => '$ 2.50',
-                        'data_cadastro' => '2017-09-27 10:15:23'
-                    )
-                ),
-                "/update/{id}" => array(
-                    "Métodos" => array("PUT"),
-                    "Resultado" => "Atualiza uma conversão no banco de dados, de acordo com o id informado, retornando o objeto atualizado ou um objeto de erro caso os dados estejam incorretos",
-                    "Exemplo de Requisição" => array(
-                        'moeda_origem' => 1,
-                        'moeda_destino' => 3,
-                        'valor' => '10.00',
-                        'cotacao' => '5.00'
-                    ),
-                    "Exemplo de Retorno" => array(
-                        'id' => 1,
-                        'moeda_origem' => 'BRL - Real Brasileiro',
-                        'moeda_destino' => 'EUR - Euro',
-                        'valor' => '10.00',
-                        'cotacao' => '5.00',
-                        'valor_convertido' => '€ 2.0',
-                        'data_cadastro' => '2017-09-27 10:15:23'
-                    )
-                ),
-                "/delete/{id}" => array(
-                    "Métodos" => array("DELETE"),
-                    "Resultado" => "Apaga a conversão referente ao id informado",
-                    "Exemplo de Retorno" => array(
-                        "sucesso" => "Conversão {id_da_conversao} removida com sucesso!"
                     )
                 )
             )
