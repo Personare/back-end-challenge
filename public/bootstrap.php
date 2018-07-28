6f0d211246823d6fed9716befc02c67f58768ef8
Diego Chavão <diegochavao@gmail.com>
2018-07-28 20:23:16 -0300

    Create a fake database in memory to run the application.

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -31,3 +31,24 @@ if (!@include($composer_autoload)) {
         }
     );
 }
+
+function createFakeDatabase() {
+    $pdo = new PDO("sqlite::memory:");
+    $pdo->exec("
+            CREATE TABLE IF NOT EXISTS currency (
+                code VARCHAR(3) PRIMARY KEY,
+                symbol VARCHAR(10) NOT NULL,
+                value DECIMAL NOT NULL,
+                base INTEGER NOT NULL DEFAULT 0
+            );
+            
+            INSERT INTO currency 
+                (code, symbol, value, base) 
+            VALUES 
+                ('USD', '$', 3.71, 0),
+                ('EUR', '€', 4.33, 0),
+                ('BRL', 'R$', 1.00, 1);
+        ");
+
+    return $pdo;
+}
