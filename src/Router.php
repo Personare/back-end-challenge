1040c76bc8d3713a4bfdd5ca8d907e84bc9d7696
Ramon Bezerra <ramonbezerra90@gmail.com>
2018-11-22 23:35:18 -0300

    creating constructor and validate of router

--- src/Router.php
+++ src/Router.php
@@ -1,5 +1,37 @@
 <?php
 
+namespace QuotationConverter;
 class Router {
+    private $method;
+    private $path;
+    private $callback;
+
+    public function __construct(string $method, string $path, $callback)
+    {
+        if($this->isValidHTTPMethod($method)){
+            $this->method = $method;
+            $this->path = $path;
+            $this->callback = $callback;
+        }else{
+            throw new \InvalidArgumentException('Invalid method');
+        }
+    }
+
+    public function getMethod() : string
+    {
+        return $this->method;
+    }
+
+    public function execute() : void
+    {
+
+    }
+
+    private function isValidHttpMethod(string $method) : bool
+    {
+        return in_array($method, ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS']);
+    }
+
+
 
 }
--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -1,19 +1,43 @@
 <?php
+namespace QuotationConverter;
 
 use \PHPUnit\Framework\TestCase;
-require_once('src/Router.php');
+use \QuotationConverter\Router;
 
 class RouterTest extends TestCase{
     private $router;
 
     public function setUp()
     {
-        $this->router = new Router();
+        $this->router = new Router('POST', '/test', function(){
+            echo json_encode([
+                'msg' => 'New route',
+            ]);
+        });
     }
 
     public function testRouterCanBeCreated()
     {
-
         $this->assertInstanceOf(Router::class, $this->router);
+    }
+
+    public function testRouterShouldHaveMethodPathAndCallback()
+    {
+        $this->assertClassHasAttribute('method', Router::class);
+        $this->assertClassHasAttribute('path', Router::class);
+        $this->assertClassHasAttribute('callback', Router::class);
+    }
+
+    public function testMethodAttributeShouldBeOneOfTheExistingHTTPMEthods()
+    {
+        $this->assertContains($this->router->getMethod(), ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS']);
+    }
+
+    public function testShouldThrowAExceptionIfMethodIsInvalid()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $router = new Router('X', '/test', function(){
+            json_encode(['err' => 'invalid method']);
+        });
     }
 }
