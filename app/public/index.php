f6598152ad594479073d283ce89af4e3357d567f
Carlos Berton <berton.5b+1@gmail.com>
2018-11-27 17:32:26 -0200

    Can choose to use CryptoCompareRepository with arg

--- app/public/index.php
+++ app/public/index.php
@@ -5,24 +5,33 @@ require '../vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
 use MoneyConverter\Domain\Service\ExchangeAPIService;
 use MoneyConverter\Domain\Repository\FixedExchangeRepository;
+use MoneyConverter\Domain\Repository\CryptoCompareExchangeRepository;
 
 
-$ratioList = array(
-	'USD' => array(
-		'BRL' => 3.900,
-		'EUR' => 0.892,
-	),
-	'EUR' => array(
-		'BRL' => 4.460,
-		'USD' => 1.120,
-	),
-	'BRL' => array(
-		'USD' => 0.256,
-		'EUR' => 0.178,
-	),
-);
+switch ($_GET['repository']) {
+	case 'cryptocompare':
+		$exchangeRepository = new CryptoCompareExchangeRepository($ratioList);
+		break;
+	default:
+		$ratioList = array(
+			'USD' => array(
+				'BRL' => 3.900,
+				'EUR' => 0.892,
+			),
+			'EUR' => array(
+				'BRL' => 4.460,
+				'USD' => 1.120,
+			),
+			'BRL' => array(
+				'USD' => 0.256,
+				'EUR' => 0.178,
+			),
+		);
 
-$exchangeRepository = new FixedExchangeRepository($ratioList);
+		$exchangeRepository = new FixedExchangeRepository($ratioList);
+		break;
+}
+
 $api = new ExchangeAPIService($exchangeRepository);
 $api->run();
 
