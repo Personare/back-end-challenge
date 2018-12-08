5e39af181f856d31a19668aca25390f2081777f3
Rafael <rafaelpviegas@gmail.com>
2018-12-08 10:36:04 -0200

    Fix phpmd

--- app/Business/CurrencyBusiness.php
+++ app/Business/CurrencyBusiness.php
@@ -4,37 +4,37 @@ namespace App\Business;
 
 class CurrencyBusiness
 {
-    public function convertCurrency($from, $to, $value)
+    public function convertCurrency($objFrom, $objTo, $value)
     {
 
-        if ($from->name == "Real") {
-            $value = number_format($value / $to->exchange_rate, 2, ".", ",");
+        if ($objFrom->name == "Real") {
+            $value = number_format($value / $objTo->exchange_rate, 2, ".", ",");
         } else {
-            $value = number_format($value * $from->exchange_rate, 2, ",", ".");
+            $value = number_format($value * $objFrom->exchange_rate, 2, ",", ".");
         }
         
         return ["value" => $value,
-                "symbol" => $to->symbol,
-                "formatted" => $to->symbol . " " . $value,
+                "symbol" => $objTo->symbol,
+                "formatted" => $objTo->symbol . " " . $value,
             ];
     }
     
-    public function validateData($from, $to, $currency_from, $currency_to, $value)
+    public function validateData($objFrom, $objTo, $currency_from, $currency_to, $value)
     {
         
-        if (empty($from)) {
+        if (empty($objFrom)) {
             return response()->json(["error" => "Currency ".$currency_from." not found!"]);
         }
         
-        if (empty($to)) {
+        if (empty($objTo)) {
             return response()->json(["error" => "Currency ".$currency_to." not found!"]);
         }
         
-        if ($from->name !== "Real" && $to->name !== "Real") {
+        if ($objFrom->name !== "Real" && $objTo->name !== "Real") {
             return response()->json(["error" => "The origin or destination currency should be Real!"]);
         }
         
-        if ($from->name == $to->name) {
+        if ($objFrom->name == $objTo->name) {
             return response()->json(["error" => "The origin and destination currencies can not be the same!"]);
         }
         
