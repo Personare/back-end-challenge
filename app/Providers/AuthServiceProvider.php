1872fffa2e64269c49d251b765feb9d33732146e
Rafael <rafaelpviegas@gmail.com>
2018-12-08 11:54:53 -0200

    Trying to fix phpmd and codeclimate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,6 +27,5 @@ class AuthServiceProvider extends ServiceProvider
         // application. The callback which receives the incoming request instance
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
-
     }
 }
