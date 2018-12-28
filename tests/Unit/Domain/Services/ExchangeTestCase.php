d312b28677155ae84d18c1aff67995b91692c17b
Iago Cavalcante <iagoangelimc@gmail.com>
2018-12-28 09:30:01 -0300

    Change type of error in test case

--- tests/Unit/Domain/Services/ExchangeTestCase.php
+++ tests/Unit/Domain/Services/ExchangeTestCase.php
@@ -31,7 +31,7 @@ class ExchangeTestCase extends TestCase
   }
 
   /**
-   * @expectedException TypeError
+   * @expectedException Exception
    */
   public function testFindQuoteFromCodeErrorWithoutArgument()
   {
