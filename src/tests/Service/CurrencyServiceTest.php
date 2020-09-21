ae75597215d27e7c08cd22b2ee5830468cb940ae
Samir Ortiz <samirortiz@gmail.com>
2020-09-21 00:32:24 -0300

    CurrencyService unit test

---
+++ src/tests/Service/CurrencyServiceTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Domain\Currency\Real;
+use App\Http\Controllers\CurrencyController;
+use App\Services\CurrencyService;
+
+class CurrencyServiceTest extends TestCase
+{
+    protected $baseUrl = 'https://localhost:8000/currency';
+
+    public function testCurrencyFail()
+    {
+        $response = $this->get('/100/brl/euro');
+        $this->assertEquals(403, $this->response->status());
+    }
+
+    public function testCurrencyCustomRate()
+    {
+        $this->get('/100/brl/eur/6.2');
+        $this->assertEquals(200, $this->response->status());
+    }
+
+    public function testCurrencyExternalRate()
+    {
+        $response = $this->get('/100/brl/eur/');
+        $this->assertEquals(200, $this->response->status());
+    }
+
+}
