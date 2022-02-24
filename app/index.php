ea02a724d19b163427b36e6618c7cb2201e2d27f
douglasor <douglasor@outlook.com>
2022-02-24 19:27:44 -0300

    CD

--- app/index.php
+++ app/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once "./vendor/autoload.php";
+require_once "/vendor/autoload.php";
 
 use App\Application\Controllers\ExchangeController;
 use App\Core\UseCase\ExchangeCurrencyUseCase;
