0164471b85fbfac38fbb2cbd3e8ad8a623c21f14
Welligton Porto <welligtonporto@gmail.com>
2023-01-19 07:43:20 -0300

    Creating service

--- src/controllers/ConvertController.js
+++ src/controllers/ConvertController.js
@@ -1,36 +1,14 @@
-const AppError = require("../utils/AppError");
-const axios = require('axios');
-
-const API_KEY = "BsHTajCuI9se8UaGXiy2XbOyTHFcB1Zl";
-
-const CURRENCY_SYMBOLS = {
-    "BRL": "R$",
-    "USD": "$",
-    "EUR": "€"
-}
+const ConvertShowService = require("../services/ConvertShowService");
 
 class ConvertController {
     async show(req, res) {
         let { from, to, amount } = req.query;
 
-        if (!from || !to || !amount){
-            throw new AppError("Os parâmetros 'from', 'to' e 'amount' são obrigatórios!");
-        }
+        const convertShowService = new ConvertShowService();
 
-        from = from.toUpperCase();
-        to = to.toUpperCase();
+        const result = await convertShowService.execute({ from, to, amount });
 
-        if (!CURRENCY_SYMBOLS.hasOwnProperty(from) || !CURRENCY_SYMBOLS.hasOwnProperty(to)){
-            throw new AppError("Moeda não permitida.");
-        }
-
-        const response = await axios.get(`https://api.apilayer.com/exchangerates_data/convert?from=${from}&to=${to}&amount=${amount}`, { 
-            headers: { 'apikey': API_KEY }
-        });
-
-        const { result } = response.data;
-
-        res.json({ value: result, symbol: CURRENCY_SYMBOLS[to] })
+        return res.json(result);
     }
 }
 
---
+++ src/services/ConvertShowService.js
@@ -0,0 +1,35 @@
+const AppError = require("../utils/AppError");
+const axios = require('axios');
+
+const API_KEY = "BsHTajCuI9se8UaGXiy2XbOyTHFcB1Zl";
+
+const CURRENCY_SYMBOLS = {
+    "BRL": "R$",
+    "USD": "$",
+    "EUR": "€"
+}
+
+class ConvertShowService {
+    async execute({ from, to, amount }){
+        if (!from || !to || !amount){
+            throw new AppError("Os parâmetros 'from', 'to' e 'amount' são obrigatórios!");
+        }
+
+        from = from.toUpperCase();
+        to = to.toUpperCase();
+
+        if (!CURRENCY_SYMBOLS.hasOwnProperty(from) || !CURRENCY_SYMBOLS.hasOwnProperty(to)){
+            throw new AppError("Moeda não permitida.");
+        }
+
+        const response = await axios.get(`https://api.apilayer.com/exchangerates_data/convert?from=${from}&to=${to}&amount=${amount}`, { 
+            headers: { 'apikey': API_KEY }
+        });
+
+        const { result } = response.data;
+
+        return { value: result, symbol: CURRENCY_SYMBOLS[to] }
+    }
+}
+
+module.exports = ConvertShowService
