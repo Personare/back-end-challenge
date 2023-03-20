e73d3a6c939d8e30022ab4520bbc8c52aa54a9c4
Tiago Oliveira <dev.tiago.oliveira@gmail.com>
2023-03-19 21:20:31 -0300

    test: ensure ConvertCurrency converts Real to Dollar

---
+++ tests/Domain/Currency/Convert/ConvertCurrencyTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Domain\Currency\Convert;
+use App\Domain\Currency\Enums\CurrencyEnum;
+use PHPUnit\Framework\TestCase;
+
+
+use App\Domain\Currency\Convert\ConvertCurrency;
+
+class ConvertCurrencyTest extends TestCase
+{
+    /**
+     * @covers
+     */
+    public function testConvertRealToDollar()
+    {
+        $convert = new ConvertCurrency(
+            CurrencyEnum::REAL,
+            CurrencyEnum::DOLAR,
+            5.28
+        );
+        $result = $convert->convert(10.0);
+
+        $this->assertEquals(1.89, $result);
+    }
+
+    
+}
