9dedd3a07831a494b27f56e56c42b73579782f38
Tiago Oliveira <dev.tiago.oliveira@gmail.com>
2023-03-19 21:57:04 -0300

    test: ensure ConvertCurrency converts Dollar to Real

--- tests/Domain/Currency/Convert/ConvertCurrencyTest.php
+++ tests/Domain/Currency/Convert/ConvertCurrencyTest.php
@@ -27,5 +27,21 @@ class ConvertCurrencyTest extends TestCase
         $this->assertEquals('US$ 1,89', $result->__toString());
     }
 
+    /**
+     * @covers
+     */
+    public function testConvertDollarToReal()
+    {
+        $convert = new ConvertCurrency(
+            CurrencyEnum::USD,
+            CurrencyEnum::BRL,
+            5.28
+        );
+        $result = $convert->convert(1000.0);
+
+        $this->assertEquals(5280, $result->getValue());
+        $this->assertEquals('R$ 5.280,00', $result->__toString());
+    }
+
     
 }
